175637c4279e6ff76a594c768a9642aaba40f063
lepton314 <le31@icloud.com>
2019-10-03 19:42:55 +0900

    10/3/2000

--- tex2mathml.py
+++ tex2mathml.py
@@ -1,3 +1,4 @@
+#モジュール読み込み
 import re
 import xml.etree.ElementTree as et
 
@@ -31,22 +32,42 @@ for formula in fixed_formulas:
             parsed_elements.append({'mode': 'function','name': 'tan'})
         elif '\+' in elements:
             parsed_elements.append({'mode': 'operator','name': '\+'})
+        elif '\pm' in elements:
+            parsed_elements.append({'mode': 'operator','name': '\+'})
         elif '\-' in elements:
             parsed_elements.append({'mode': 'operator','name': '\-'})
-        elif '\=' in elements:
-            parsed_elements.append({'mode': 'equal','name': '\='})
+        elif '=' in elements:
+            parsed_elements.append({'mode': 'equal','name': '='})
         else:
             parsed_elements.append({'mode': 'number','value': element})
             parsed_formulas.append(parsed_elements)
+    print(parsed_formulas)
+
+
 #xmlに変換
 mathmls = []
 math = et.Element('math')
 tree = et.ElementTree(element=math)
 for formulas in parsed_formulas:
-    if 'operator' in formulas.keys('mode'):
+    if 'operator' in formulas.get('mode'):　#演算子
         operator = et.SubElement(math,'mo')
-        mo.text = formulas.get('name')
-    elif 'number' in formulas.keys('mode'):
-        if str.isdecimal(formulas.get('value'):
-            number = et.SubElement(math,'mn'):
+        operator.text = formulas.get('name')
+    if 'equal' in formulas.get('mode'):　#演算子
+        equal = et.SubElement(math,'mo')
+        equal.text = formulas.get('name')
+    elif 'number' in formulas.get('mode'): #英数字
+        if str.isdecimal(formulas.get('value')):#数字のみ
+            number = et.SubElement(math,'mn')
             mn.text = formulas.get('value')
+        else:
+            temp = re.search(r'\d+',forlamus.get('value')) #混在した場合の数字の取り出し。
+            number = et.SubElement(math,'mn')
+            mn.text = temp
+            temp2 = list(re.search('[^0-9]+',forlamus.get('value')))#英字部分取り出し
+            for String in temp2:
+                Inti = et.SubElement(math,'mo')
+                Inti.text = '&InvisibleTimes;'
+                string = et.SubElement(math,'mi')
+                string.text = String
+        
+tree.write('test.xml', encoding='utf-8', xml_declaration=True) #
