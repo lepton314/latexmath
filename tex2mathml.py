163b4bc187bce8eb79975dff054fc57766e59e04
lepton314 <le31@icloud.com>
2019-10-09 21:51:42 +0900

    10092200

--- tex2mathml.py
+++ tex2mathml.py
@@ -25,16 +25,23 @@ for formula in fixed_formulas:
     for element in elements_with_empty:
         if len(element) > 0:
             elements.append(element)
-    print(formula)
-    print(trimmed_formula)
-    print(elements)
+    #print(formula)
+    #print(trimmed_formula)
+    #print(elements)
     parsed_elements = []
     for element in elements:
         parse_element(element, parsed_elements)
-
     parsed_formulas.append(parsed_elements)
     print(parsed_formulas)
-        
+#Subformula
+built_formulas = []
+built_formula = []
+for formula in parsed_formulas:
+    for element in formula:
+        built_formula.append(element)
+    print(formula) 
+    print(built_formula)
+    built_formulas.append(built_formula)
 #xmlに変換
 xmlcount = 0
 for formula in parsed_formulas:
@@ -61,6 +68,7 @@ for formula in parsed_formulas:
             brakets -= 1
         elif 'sqrt' in element['mode']:
             sqrtcount += 1
+            sqrt = et.SubElement(root,'msqrt')
         elif 'root' in element['mode']:
             rootcount += 1
         elif 'frac' in element['mode']:
@@ -70,31 +78,31 @@ for formula in parsed_formulas:
         elif 'sup' in element['mode']:
             supcount += 1        
         elif brakets == 0:
-                if 'operator2' in element['mode']:#演算子
-                    IVcount = 0
-                    operator = et.SubElement(root,'mo')
-                    operator.text = element['name']
-                elif 'function' == element['mode']:
-                    IVcount = 0
-                    function = et.SubElement(root,'mi')
-                    function.text = element['name']
+            if 'operator2' in element['mode']:#演算子
+                IVcount = 0
+                operator = et.SubElement(root,'mo')
+                operator.text = element['name']
+            elif 'function' == element['mode']:
+                IVcount = 0
+                function = et.SubElement(root,'mi')
+                function.text = element['name']
+                Inti = et.SubElement(root,'mo')
+                Inti.text = '&InvisibleTimes;'
+            elif 'symbol'in element['mode']: #二項関係記号
+                IVcount = 0
+                symbol = et.SubElement(root,'mo')
+                symbol.text = element['name']
+            elif 'number' in element['mode']: #数字
+                number = et.SubElement(root,'mn')
+                number.text = element['value']
+                IVcount += 1
+            else:
+                if IVcount >= 1: 
                     Inti = et.SubElement(root,'mo')
-                    Inti.text = '&InvisibleTimes;'
-                elif 'symbol'in element['mode']: #二項関係記号
-                    IVcount = 0
-                    symbol = et.SubElement(root,'mo')
-                    symbol.text = element['name']
-                elif 'number' in element['mode']: #数字
-                    number = et.SubElement(root,'mn')
-                    number.text = element['value']
-                    IVcount += 1
-                else:
-                    if IVcount >= 1: 
-                        Inti = et.SubElement(root,'mo')
-                        Inti.text = '&#x2062;<!--INVISIBLE TIMES-->'
-                        IVcount -= 1
-                    characters = et.SubElement(root,'mi')
-                    characters.text = element['value']
-                    IVcount += 1
+                    Inti.text = '&#x2062;<!--INVISIBLE TIMES-->'
+                    IVcount -= 1
+                characters = et.SubElement(root,'mi')
+                characters.text = element['value']
+                IVcount += 1           
     xmlcount += 1
     tree.write('output/math' + str(xmlcount) + '.xml', encoding='utf-8', xml_declaration=True) #xmlとしての書き出し
